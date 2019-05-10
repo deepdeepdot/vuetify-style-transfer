00ebe3b4a7a0d350df5e00a1747529244d580045
Pedro Ha <ph@educreational.com>
2019-05-10 11:49:16 -0400

    Layout for CameraModal

--- src/components/CameraModal.vue
+++ src/components/CameraModal.vue
@@ -1,23 +1,41 @@
-
 <template>
   <v-layout row justify-center>
+    <v-dialog v-model="dialog" persistent width="580">
+      <div>
+          <v-toolbar
+            color="pink"
+            dark
+          >
+            <v-toolbar-title>Take a Snapshot!!</v-toolbar-title>
+          </v-toolbar>
 
-    <v-dialog v-model="dialog" persistent max-width="700" max-height="600">
-      <v-card>
-        <v-card-title class="headline">Take a snapshot!</v-card-title>
+          <v-card>
+            <v-container
+              fluid
+              grid-list-lg
+            >
+              <v-layout row wrap>
 
-        <v-card-media>
-          <video id="webcam-video" width="500" height="375"></video>
-        </v-card-media>
+                <v-flex xs12 class="centered">
+                    <video id="webcam-video" width="500" height="375"></video>
+                </v-flex>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" @click="dialog = false">Abort abort...</v-btn>
-          <v-btn color="primary" @click="snap">Snap!!</v-btn>
-        </v-card-actions>
-      </v-card>
+                <v-flex xs12>
+                  <v-card flat>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="primary" @click="dialog = false">Cancel</v-btn>
+                      <v-btn color="primary" @click="snap">Snap!!</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-flex>
+
+              </v-layout>
+            </v-container>
+          </v-card>
+
+        </div>
     </v-dialog>
-
   </v-layout>
 </template>
 
@@ -56,3 +74,25 @@ export default {
 }
 
 </script>
+
+<style>
+
+video {
+  border: 2px solid lime;
+  margin: auto;
+}
+
+.filler {
+  display: block;
+  height: 20px;
+}
+
+.centered {
+  text-align:center;
+}
+
+.centered > * {
+  display: inline-block;
+}
+
+</style>
