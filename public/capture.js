0ed0ae7696ea58519003f020089d5383b9246167
Pedro Ha <ph@educreational.com>
2019-05-15 18:32:10 -0400

    Add standalone html test pages for image capture in mobiles

--- public/capture.js
+++ public/capture.js
@@ -1,100 +1,157 @@
-(function () {
-    // The width and height of the captured photo. We will set the
-    // width to the value defined here, but the height will be
-    // calculated based on the aspect ratio of the input stream.
+var isMobile = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
 
-    var width = 320;    // We will scale the photo width to this
-    var height = 0;     // This will be computed based on the input stream
+// The width and height of the captured photo. We will set the
+// width to the value defined here, but the height will be
+// calculated based on the aspect ratio of the input stream.
 
-    // |streaming| indicates whether or not we're currently streaming
-    // video from the camera. Obviously, we start at false.
+var width = isMobile ? 320 : 400;    // We will scale the photo width to this
+var height = 0;     // This will be computed based on the input stream
 
-    var streaming = false;
+var streaming = false;
+var video = null;
+var canvas = document.createElement('canvas');
+var videoStream = null;
 
-    // The various HTML elements we need to configure or control. These
-    // will be set by the startup() function.
+// TODO: remove 'Take picture' option of navigator.mediaDevices is not defined
 
-    var video = null;
-    var canvas = null;
-    var photo = null;
-    var startbutton = null;
+function getUserMedia(constraints) {
+    let callback = function (stream) {
+        video.srcObject = stream;
+        video.play();
+        videoStream = stream;
+    };
+    let errorCallback = function (error) {
+        if (error.name === 'ConstraintNotSatisfiedError') {
+            return new Error('The resolution ' + constraints.video.width.exact + 'x' +
+                constraints.video.width.exact + ' px is not supported by your device.');
+        } else if (error.name === 'PermissionDeniedError') {
+            return new Error('Permissions have not been granted to use your camera and ' +
+                'microphone, you need to allow the page access to your devices in ' +
+                'order for the demo to work.');
+        }
+        return new Error('activateCamera error: ' + error.name);
+    };
+    // Deprecated API: navigator.getUserMedia(), still not working in iphone6
+    //
+    // const navigatorGetUserMedia = navigator.getUserMedia ||
+    //     navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
+    //     navigator.msGetUserMedia;
+    // return navigatorGetUserMedia.call(navigator, constraints, callback, errorCallback);
+    return navigator.mediaDevices.getUserMedia(constraints)
+        .then(callback)
+        .catch(errorCallback);
+}
 
-
-    function startup() {
-        video = document.getElementById('webcam-video');
-        canvas = document.getElementById('canvas');
-        photo = document.getElementById('photo');
-        startbutton = document.getElementById('startbutton');
-
-        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-            .then(function (stream) {
-                video.srcObject = stream;
-                video.play();
-            })
-            .catch(function (err) {
-                console.log("An error occurred: " + err);
-            });
-
-        video.addEventListener('canplay', function (ev) {
-            if (!streaming) {
-                height = video.videoHeight / (video.videoWidth / width);
-
-                // Firefox currently has a bug where the height can't be read from
-                // the video, so we will make assumptions if this happens.
-
-                if (isNaN(height)) {
-                    height = width / (4 / 3);
-                }
-
-                video.setAttribute('width', width);
-                video.setAttribute('height', height);
-                canvas.setAttribute('width', width);
-                canvas.setAttribute('height', height);
-                streaming = true;
-            }
-        }, false);
-
-        startbutton.addEventListener('click', function (ev) {
-            takepicture();
-            ev.preventDefault();
-        }, false);
-
-        clearphoto();
+function activateCamera(mediaConstraints) {
+    // Make sure to return a Promise in all scenarios
+    if (typeof navigator.mediaDevices === 'undefined') {
+        return Promise.reject('Cannot access camera device: mediaDevices');
     }
-
-    // Fill the photo with an indication that none has been
-    // captured.
-
-    function clearphoto() {
-        var context = canvas.getContext('2d');
-        context.fillStyle = "#AAA";
-        context.fillRect(0, 0, canvas.width, canvas.height);
-
-        var data = canvas.toDataURL('image/png');
-        photo.setAttribute('src', data);
+    if (!videoStream) {
+        var constraints = {
+            ...mediaConstraints,
+            audio: false,
+            video: true
+        };
+        return getUserMedia(constraints);
     }
+    return Promise.resolve({});
+}
 
-    // Capture a photo by fetching the current contents of the video
-    // and drawing it into a canvas, then converting that to a PNG
-    // format data URL. By drawing it on an offscreen canvas and then
-    // drawing that to the screen, we can change its size and/or apply
-    // other changes before drawing it.
+function deactivateCamera() {
+    if (videoStream) {
+        videoStream.getTracks().forEach(track => track.stop())
+    }
+    videoStream = null;
+}
 
-    function takepicture() {
+function takePicture(photo, resizePhoto) {
+    if (videoStream) {
         var context = canvas.getContext('2d');
         if (width && height) {
+            canvas.style.setProperty('width', width + 'px');
+            canvas.style.setProperty('height', height + 'px');
             canvas.width = width;
             canvas.height = height;
             context.drawImage(video, 0, 0, width, height);
 
             var data = canvas.toDataURL('image/png');
             photo.setAttribute('src', data);
-        } else {
-            clearphoto();
+            if (resizePhoto) {
+                photo.style.setProperty('width', width + 'px');
+                photo.style.setProperty('height', height + 'px');
+            }
+            return {
+                width,
+                height,
+            }
         }
     }
+    else {
+        clearphoto(photo);
+    }
+}
 
-    // Set up our event listener to run the startup process
-    // once loading is complete.
-    window.addEventListener('load', startup, false);
-})();
+function clearphoto(photo) {
+    var context = canvas.getContext('2d');
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    var data = canvas.toDataURL('image/png');
+    photo.setAttribute('src', data);
+}
+
+function initCamera(theVideo) {
+    video = theVideo;
+
+    video.addEventListener('canplay', function (/* ev */) {
+        if (!streaming) {
+            height = video.videoHeight / (video.videoWidth / width);
+            // Firefox currently has a bug where the height can't be read from
+            // the video, so we will make assumptions if this happens.
+            if (isNaN(height)) {
+                height = width / (4 / 3);
+            }
+            video.style.setProperty('width', width + 'px');
+            video.style.setProperty('height', height + 'px');
+            streaming = true;
+        }
+    }, false);
+}
+
+function startup() {
+    var startButton = document.getElementById('startButton');
+    var pictureButton = document.getElementById('pictureButton');
+    var stopButton = document.getElementById('stopButton');
+    var photo = document.getElementById('photo');
+    var theVideo = document.getElementById('webcam-video');
+
+    initCamera(theVideo);
+
+    startButton.addEventListener('click', function (ev) {
+        activateCamera();
+        ev.preventDefault();
+    }, false);
+
+    pictureButton.addEventListener('click', function (ev) {
+        takePicture(photo, true);
+        ev.preventDefault();
+    }, false);
+
+    stopButton.addEventListener('click', function (ev) {
+        deactivateCamera();
+        ev.preventDefault();
+    });
+
+    clearphoto(photo);
+}
+
+// Capture a photo by fetching the current contents of the video
+// and drawing it into a canvas, then converting that to a PNG
+// format data URL. By drawing it on an offscreen canvas and then
+// drawing that to the screen, we can change its size and/or apply
+// other changes before drawing it.
+
+// Set up our event listener to run the startup process
+// once loading is complete.
+window.addEventListener('load', startup, false);
